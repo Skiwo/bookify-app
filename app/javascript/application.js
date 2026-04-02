d910ca16f97632d9e2a96d7a84845f853f1faff0
Gautam Chandna <gautam@skiwo.com>
2026-04-02 08:05:18 +0530

    Booking form fix

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,4 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-import * as bootstrap from "bootstrap"
+// Bootstrap JS is loaded from CDN in layouts (see booker/freelancer/application); no vendored ESM bundle in repo.
