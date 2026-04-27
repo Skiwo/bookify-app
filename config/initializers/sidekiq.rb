cfebc81b2496cca7b05ff91d8a66dcbb39230967
Nikolay Kalashnikov <nikolay.klsn@gmail.com>
2026-04-27 11:58:40 +0300

    fix redis initialization on heroku

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
-Sidekiq.configure_server do |config|
-  config.redis = { url: ENV.fetch("REDIS_URL", "redis://localhost:6379/0") }
-end
+redis_config = {
+  url: ENV.fetch("REDIS_URL", "redis://localhost:6379/0"),
+  ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
+}
 
-Sidekiq.configure_client do |config|
-  config.redis = { url: ENV.fetch("REDIS_URL", "redis://localhost:6379/0") }
-end
+Sidekiq.configure_server { |c| c.redis = redis_config }
+Sidekiq.configure_client { |c| c.redis = redis_config }
